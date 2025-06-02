e334f39a4d81007924867487ddd55345a0885828
Kaua <kaua@core>
2025-06-02 16:41:44 -0300

    feat: dealing with text qrCode

--- App.tsx
+++ App.tsx
@@ -1,24 +1,49 @@
-import { useState } from 'react';
-import { StyleSheet, Button, View, Modal } from 'react-native';
+import { useState, useRef } from 'react';
+import { StyleSheet, Button, View, Modal, Alert } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera';
 
 
 export default function App() {
   const [modalIsVisible, setModalIsVisible] = useState<boolean>(false);
+  const [permission, requestPermission] = useCameraPermissions();
 
+  const qrCodeLock = useRef(false);
 
   const handleOpenCamera = async () => {
     try{
-      setModalIsVisible(true)
+      const {granted} = await requestPermission()
+    
+      if(!granted){
+        return Alert.alert("Camera:", "you need to allow access to the camera.")
+      }
+
+      setModalIsVisible(true);
+      qrCodeLock.current = false;
     }catch (erro) {
       console.log(erro)
     }
   }
+
+
+  const handleQRCodeRead = (data: string) => {
+    setModalIsVisible(false);
+    Alert.alert("QrCode data:", data);
+  }
+
   return (
     <View style={styles.container}>
         <Button title='Read QRCode' onPress={handleOpenCamera}></Button>
         <Modal visible={modalIsVisible} style={{flex: 1}}>
-          <CameraView style={{flex: 1}} facing='back' />
+          <CameraView 
+          style={{flex: 1}} 
+          facing='back' 
+          onBarcodeScanned={({data}) => {
+            if(data && !qrCodeLock.current){
+              qrCodeLock.current = true;
+              setTimeout(() => handleQRCodeRead(data), 500);
+            }
+          }}
+          />
           <View style={styles.footer}>
             <Button title='Cancel' onPress={() => setModalIsVisible(false)} />
           </View>
